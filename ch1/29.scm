d1928b2ea29969dfd6e763913507c47e70d83776
Jennifer Reiber Kyle <reiberkyle@gmail.com>
2015-02-16 09:36:27 -0600

    Update and rename 29.scm to 29.md

--- ch1/29.scm
+++
@@ -1,3 +0,0 @@
-; Define a procedure that takes as arguments f, a, b, n and returns the value of the integral
-; calculated using Simpson's Rule. Integrate cube between 0 and 1 with n=100 and n=1000 and compare results
-; to integral procedure above
