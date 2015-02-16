018e8dc1cef4b3a4c6407dc7ed7d2e01fef131a4
Jennifer Reiber Kyle <reiberkyle@gmail.com>
2015-02-15 19:55:45 -0600

    add ex1p3.scm

---
+++ ex1p3.scm
@@ -0,0 +1,10 @@
+(define (sum-of-squares a b)
+    (+ (* a a) (* b b))
+)
+
+(define (sum-of-squares-of-largest-two a b c)
+    (cond ((and (>= a c) (>= b c)) (sum-of-squares a b))
+          ((and (>= a b) (>= c b)) (sum-of-squares a c))
+          (else (sum-of-squares b c))
+    )
+)
